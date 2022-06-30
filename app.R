5f8ba5a615f2d1f8acd12e9e43091044c96290f7
UO258007 <UO258007@uniovi.es>
2022-06-30 19:56:26 +0200

    Ya se puede subir a la app los ficheros editados por la misma

--- app.R
+++ app.R
@@ -61,7 +61,7 @@ ui3 <- bootstrapPage(
            style="font-weight: bold; font-size: 14px"),
       tags$hr(),
       downloadButton("download","Download file", style='width: 150px'),
-      #DT::dataTableOutput("table2")
+      DT::dataTableOutput("table2")
     ),
   mainPanel(width = 9,
            DT::dataTableOutput("table")
@@ -87,13 +87,14 @@ server3 <- function(input, output){
                     blank.lines.skip = FALSE,
                     col.names = c('Word','BIO'))
     
-    d$Word <- replace(d$Word, d$Word  == "", "\n\n")
+    d$Word <- replace(d$Word, d$Word  == "", "\n")
+    
     l <- length(d$Word)
     vec <- 1:l
     d$AbstractID <- vec 
     
     temp = 1
-    cond <- c(FALSE, (d[-nrow(d),1] == "\n\n"))
+    cond <- c(FALSE, (d[-nrow(d),1] == "\n"))
     r <- d[,3]
     for(i in 1:length(d$AbstractID)){
       r[i] <- temp
@@ -104,17 +105,15 @@ server3 <- function(input, output){
     
     d[,3]  <- r
     d_disp <- d
-    nabs <- length(which(d_disp$Word == "\n\n"))
+    nabs <- length(which(d_disp$Word == "\n"))
     ids <- 1:nabs
     p <- data.frame(AbstractID = ids, 
                     Content = strsplit(do.call(
-                      paste, c(d_disp$Word, list(collapse=","))), "\n\n"
+                      paste, c(d_disp$Word, list(collapse=","))), "\n"
                     ), 
                     Ntokens = 0, 
                     NChanges = 0)
     colnames(p) = c("ID","Content","NTokens","NChanges")
-
- 
     
     # ----- Highlight Keys ------
     cont=1
@@ -131,21 +130,22 @@ server3 <- function(input, output){
       }
       cont = cont + 1
     }
-    
-    p <- data.frame(AbstractID= ids, 
+  
+    p <- data.frame(AbstractID= ids,
                     Content = strsplit(
-                      do.call(paste, c(d_disp$Word, list(collapse=","))), "\n\n"), 
-                    Ntokens =  str_count(p$Content, '\\s+')+1, 
+                    do.call(paste, c(d_disp$Word, list(collapse=","))), "\n"),
+                    Ntokens =  str_count(p$Content, '\\s+')+1,
                     NChanges = 0)
+
     colnames(p) = c("ID","Content","NTokens","NChanges")
     
     # Guardamos los dataframes resultantes en una lista:
     # A -> Dataframe con los datos, el que se guardará (sin tags html)
     # B -> Dataframe con los abstracts preprocesados (lo que verá el usuario) 
     # C -> Dataframe para el display que lleva las etiquetas html (no se descarga)
+    
     combo <- list(a = d, b = p, c = d_disp)
     vals$x <- combo
-    
     
       },
     error=function(cond){
@@ -154,10 +154,10 @@ server3 <- function(input, output){
   })
   
   # ------- Mostrar la tabla ------------
-  # TABLA PROVISIONAL PARA VER LOS CAMBIOS EN EL FICHERO
-  # output$table2 = DT::renderDataTable({
-  #   DT::datatable(vals$x[['a']],escape = FALSE)
-  # })
+  #TABLA PROVISIONAL PARA VER LOS CAMBIOS EN EL FICHERO
+  output$table2 = DT::renderDataTable({
+    DT::datatable(vals$x[['a']],escape = FALSE)
+  })
   
   output$table <- DT::renderDataTable({
     DT::datatable(vals$x[['b']],
@@ -190,7 +190,7 @@ server3 <- function(input, output){
               '<span style="background-color:#FDFF82">',vals$x[['a']]$Word[i],'</span>'
             )
           }
-          vals$x[['b']]$Content[absid] <- strsplit(do.call(paste, c(vals$x[['c']]$Word[vals$x[['c']]$AbstractID == absid], list(collapse=","))), "\n\n") 
+          vals$x[['b']]$Content[absid] <- strsplit(do.call(paste, c(vals$x[['c']]$Word[vals$x[['c']]$AbstractID == absid], list(collapse=","))), "\n") 
           vals$x[['b']]$NChanges[absid] <- vals$x[['b']]$NChanges[absid] + 1
           
           msg("Success!")
@@ -220,13 +220,14 @@ server3 <- function(input, output){
     }
     else{
       for(i in index){
-        if(vals$x[['a']]$BIO[i]=="I-KEY" || vals$x[['a']]$BIO[i]=="B-KEY")
+        if(vals$x[['a']]$BIO[i-1]=="I-KEY" || vals$x[['a']]$BIO[i-1]=="B-KEY"){
         vals$x[['a']]$BIO[i] <- "I-KEY"
         vals$x[['c']]$Word[i]<-paste0(
           '<span style="background-color:#D6EEEE">',vals$x[['a']]$Word[i],'</span>')
+        }
       }
       
-      vals$x[['b']]$Content[absid] <- strsplit(do.call(paste, c(vals$x[['c']]$Word[vals$x[['c']]$AbstractID == absid], list(collapse=","))), "\n\n") 
+      vals$x[['b']]$Content[absid] <- strsplit(do.call(paste, c(vals$x[['c']]$Word[vals$x[['c']]$AbstractID == absid], list(collapse=","))), "\n") 
       vals$x[['b']]$NChanges[absid] <- vals$x[['b']]$NChanges[absid] + 1
       
       msg("Success!")
@@ -253,19 +254,20 @@ server3 <- function(input, output){
         arr.ind = TRUE)
     
       absid <- input$table_rows_selected
-      if(vals$x[['a']]$BIO[index[1]]=="O"){
-        msg("Token already tagged as O")
-      }
-      else{
+      
+      # if(vals$x[['a']]$BIO[index[1]]=="O"){
+      #   msg("Token already tagged as O")
+      # }
+      #else{
         for(i in index){
           vals$x[['a']]$BIO[i] <- "O"
           vals$x[['c']]$Word[i] <- paste0(vals$x[['a']]$Word[i])
         }
-        vals$x[['b']]$Content[absid] <- strsplit(do.call(paste, c(vals$x[['c']]$Word[vals$x[['c']]$AbstractID == absid], list(collapse=","))), "\n\n") 
+        vals$x[['b']]$Content[absid] <- strsplit(do.call(paste, c(vals$x[['c']]$Word[vals$x[['c']]$AbstractID == absid], list(collapse=","))), "\n") 
         vals$x[['b']]$NChanges[absid] <- vals$x[['b']]$NChanges[absid] + 1
         
         msg("Success!")
-      }
+      #}
     },
     error=function(cond){
       msg("ERROR: Word not found, please try again")
@@ -282,10 +284,15 @@ server3 <- function(input, output){
   output$download <- downloadHandler(
     filename = "BIOFile.txt",
     content = function(filed) {
+      vals$x[['a']]$Word[vals$x[['a']]$Word == "\n"] <- ""
+      vals$x[['a']]$BIO[vals$x[['a']]$BIO == "\n"] <- ""
+      
       write.table(vals$x[['a']][, c("Word", "BIO")], 
                 filed,
                 row.names = FALSE,
-                col.names = FALSE)
+                col.names = FALSE,
+                quote = FALSE,
+                sep = '\t')
     }
   )
 }
